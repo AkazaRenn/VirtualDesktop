4aff63dab1a3fd1312eb744aea3da81485721f9b
Renn <8340896+AkazaRenn@users.noreply.github.com>
2023-11-12 14:03:21 -0500

    Maximize to new desktop test

--- samples/VirtualDesktop.Showcase/MainWindow.xaml.cs
+++ samples/VirtualDesktop.Showcase/MainWindow.xaml.cs
@@ -16,68 +16,166 @@ using WindowsDesktop;
 
 namespace VirtualDesktopShowcase;
 
-partial class MainWindow
-{
+partial class MainWindow {
     private const int _delay = 2000;
     private IDisposable? _applicationViewChangedListener;
 
     public ObservableCollection<VirtualDesktopViewModel> Desktops { get; } = new();
 
-    public MainWindow()
-    {
-        this.InitializeComponent();
-        this.InitializeComObjects();
+    readonly Dictionary<IntPtr, VirtualDesktop> desktopsMap = new();
+    readonly List<VirtualDesktop> desktopList = new();
+
+    WindowTracker windowTracker;
+
+    bool NotReservedDesktops(Guid id) {
+        return desktopList[0].Id != id;// && desktops[1].Id != id;
     }
 
-    private void InitializeComObjects()
-    {
+    static void OnFloatWindow(IntPtr hwnd) {
+        try {
+            if(!VirtualDesktop.IsPinnedWindow(hwnd)) {
+                VirtualDesktop.PinWindow(hwnd);
+            }
+        } catch { }
+    }
+
+    void OnMax(IntPtr hwnd, string fileDescription) {
+        try {
+            var desktop = VirtualDesktop.Create();
+            desktop.Name = fileDescription;
+            System.Threading.Thread.Sleep(400);
+            if(VirtualDesktop.IsPinnedWindow(hwnd)) {
+                VirtualDesktop.UnpinWindow(hwnd);
+            }
+
+            VirtualDesktop.MoveToDesktop(hwnd, desktop);
+            if(VirtualDesktop.FromHwnd(hwnd) == VirtualDesktop.Current) {
+                desktop.Switch();
+            }
+            desktopsMap.Add(hwnd, desktop);
+        } catch { }
+    }
+
+    void OnUnmax(IntPtr hwnd) {
+        try {
+            System.Threading.Thread.Sleep(200);
+            var curr = VirtualDesktop.FromHwnd(hwnd);
+
+            VirtualDesktop.PinWindow(hwnd);
+            if(curr == VirtualDesktop.Current) {
+                desktopList[0].Switch();
+            }
+
+            if(curr != null && NotReservedDesktops(curr.Id)) {
+                curr.Remove();
+            }
+
+            desktopsMap.Remove(hwnd);
+        } catch { }
+    }
+
+    void OnMin(IntPtr hwnd) {
+        try {
+            var curr = VirtualDesktop.FromHwnd(hwnd);
+            desktopList[0].Switch();
+            if(curr != null && NotReservedDesktops(curr.Id)) {
+                curr.Remove();
+            }
+
+            desktopsMap.Remove(hwnd);
+        } catch {
+        }
+    }
+
+    void OnClose(IntPtr hwnd) {
+        try {
+            System.Threading.Thread.Sleep(100);
+            if(NotReservedDesktops(VirtualDesktop.Current.Id)) {
+                VirtualDesktop.Current.Remove();
+                desktopList[0].Switch();
+            }
+
+            desktopsMap.Remove(hwnd);
+        } catch {
+        }
+    }
+
+    void ClearDesktops() {
+        foreach(var desktop in desktopsMap.Values) {
+            desktop.Remove();
+        }
+    }
+
+
+    public MainWindow() {
+        InitializeComponent();
+        InitializeComObjects();
+        InitializeWindowTracker();
+    }
+
+    private void InitializeWindowTracker() {
+        var currDesktop = VirtualDesktop.Current;
+
+        windowTracker = new();
+        windowTracker.FloatWindowEvent += (_, e) => OnFloatWindow(e.hwnd);
+        windowTracker.MaximizeEvent += (_, e) => OnMax(e.hwnd, e.description);
+        windowTracker.UnmaximizeEvent += (_, e) => OnUnmax(e.hwnd);
+        windowTracker.MinimizeEvent += (_, e) => OnMin(e.hwnd);
+        windowTracker.CloseEvent += (_, e) => OnClose(e.hwnd);
+        windowTracker.SortCurrentWindows();
+
+        currDesktop.Switch();
+    }
+
+    private void InitializeComObjects() {
         VirtualDesktop.Configure();
 
-        VirtualDesktop.Created += (_, desktop) =>
-        {
-            this.Dispatcher.Invoke(() =>
-            {
+        VirtualDesktop.Created += (_, desktop) => {
+            this.Dispatcher.Invoke(() => {
                 this.Desktops.Add(new VirtualDesktopViewModel(desktop));
+                this.desktopList.Add(desktop);
                 Debug.WriteLine($"Created: {desktop.Name}");
             });
         };
 
-        VirtualDesktop.CurrentChanged += (_, args) =>
-        {
+        VirtualDesktop.CurrentChanged += (_, args) => {
             foreach(var desktop in this.Desktops)
                 desktop.IsCurrent = desktop.Id == args.NewDesktop.Id;
             Debug.WriteLine($"Switched: {args.OldDesktop.Name} -> {args.NewDesktop.Name}");
         };
 
-        VirtualDesktop.Moved += (_, args) =>
-        {
-            this.Dispatcher.Invoke(() =>
-            {
+        VirtualDesktop.Moved += (_, args) => {
+            this.Dispatcher.Invoke(() => {
                 this.Desktops.Move(args.OldIndex, args.NewIndex);
+                this.desktopList.RemoveAt(args.OldIndex);
+                this.desktopList.Insert(args.NewIndex, args.Desktop);
                 Debug.WriteLine($"Moved: {args.OldIndex} -> {args.NewIndex}, {args.Desktop}");
             });
         };
 
-        VirtualDesktop.Destroyed += (_, args) =>
-        {
-            this.Dispatcher.Invoke(() =>
-            {
+        VirtualDesktop.Destroyed += (_, args) => {
+            this.Dispatcher.Invoke(() => {
                 var target = this.Desktops.FirstOrDefault(x => x.Id == args.Destroyed.Id);
-                if(target != null)
+                if(target != null) {
                     this.Desktops.Remove(target);
+                }
+                foreach(var desktop in this.desktopList) {
+                    if(desktop.Id == args.Destroyed.Id) {
+                        desktopList.Remove(desktop);
+                        break;
+                    }
+                }
             });
         };
 
-        VirtualDesktop.Renamed += (_, args) =>
-        {
+        VirtualDesktop.Renamed += (_, args) => {
             var desktop = this.Desktops.FirstOrDefault(x => x.Id == args.Desktop.Id);
             if(desktop != null)
                 desktop.Name = args.Name;
             Debug.WriteLine($"Renamed: {args.Desktop}");
         };
 
-        VirtualDesktop.WallpaperChanged += (_, args) =>
-        {
+        VirtualDesktop.WallpaperChanged += (_, args) => {
             var desktop = this.Desktops.FirstOrDefault(x => x.Id == args.Desktop.Id);
             if(desktop != null)
                 desktop.WallpaperPath = new Uri(args.Path);
@@ -86,27 +184,23 @@ partial class MainWindow
 
         var currentId = VirtualDesktop.Current.Id;
 
-        foreach(var desktop in VirtualDesktop.GetDesktops())
-        {
+        foreach(var desktop in VirtualDesktop.GetDesktops()) {
             var vm = new VirtualDesktopViewModel(desktop);
             if(desktop.Id == currentId)
                 vm.IsCurrent = true;
 
             this.Desktops.Add(vm);
+            this.desktopList.Add(desktop);
         }
     }
 
-    protected override void OnSourceInitialized(EventArgs e)
-    {
+    protected override void OnSourceInitialized(EventArgs e) {
         base.OnSourceInitialized(e);
 
-        this._applicationViewChangedListener = VirtualDesktop.RegisterViewChanged(this.GetHandle(), handle =>
-        {
-            this.Dispatcher.Invoke(() =>
-            {
+        this._applicationViewChangedListener = VirtualDesktop.RegisterViewChanged(this.GetHandle(), handle => {
+            this.Dispatcher.Invoke(() => {
                 var parent = VirtualDesktop.FromHwnd(handle);
-                foreach(var desktop in this.Desktops)
-                {
+                foreach(var desktop in this.Desktops) {
                     desktop.ShowcaseMessage = parent == null
                         ? "(this window is pinned)"
                         : desktop.Id == parent.Id
@@ -117,24 +211,21 @@ partial class MainWindow
         });
     }
 
-    protected override void OnClosed(EventArgs e)
-    {
+    protected override void OnClosed(EventArgs e) {
         this._applicationViewChangedListener?.Dispose();
+        ClearDesktops();
         base.OnClosed(e);
     }
 
     private void CreateNew(object sender, RoutedEventArgs e)
         => VirtualDesktop.Create();
 
-    private async void CreateNewAndMove(object sender, RoutedEventArgs e)
-    {
+    private async void CreateNewAndMove(object sender, RoutedEventArgs e) {
         var desktop = VirtualDesktop.Create();
 
-        if(this.ThisWindowMenu.IsChecked ?? true)
-        {
+        if(this.ThisWindowMenu.IsChecked ?? true) {
             desktop.SwitchAndMove(this);
-        } else
-        {
+        } else {
             await Task.Delay(_delay);
 
             var handle = GetForegroundWindow();
@@ -144,22 +235,18 @@ partial class MainWindow
         }
     }
 
-    private void SwitchLeft(object sender, RoutedEventArgs e)
-    {
+    private void SwitchLeft(object sender, RoutedEventArgs e) {
         VirtualDesktop.Current.GetLeft()?.Switch();
     }
 
-    private async void SwitchLeftAndMove(object sender, RoutedEventArgs e)
-    {
+    private async void SwitchLeftAndMove(object sender, RoutedEventArgs e) {
         var left = VirtualDesktop.Current.GetLeft();
         if(left == null)
             return;
 
-        if(this.ThisWindowMenu.IsChecked ?? true)
-        {
+        if(this.ThisWindowMenu.IsChecked ?? true) {
             left.SwitchAndMove(this);
-        } else
-        {
+        } else {
             await Task.Delay(_delay);
 
             var handle = GetForegroundWindow();
@@ -169,22 +256,18 @@ partial class MainWindow
         }
     }
 
-    private void SwitchRight(object sender, RoutedEventArgs e)
-    {
+    private void SwitchRight(object sender, RoutedEventArgs e) {
         VirtualDesktop.Current.GetRight()?.Switch();
     }
 
-    private async void SwitchRightAndMove(object sender, RoutedEventArgs e)
-    {
+    private async void SwitchRightAndMove(object sender, RoutedEventArgs e) {
         var right = VirtualDesktop.Current.GetRight();
         if(right == null)
             return;
 
-        if(this.ThisWindowMenu.IsChecked ?? true)
-        {
+        if(this.ThisWindowMenu.IsChecked ?? true) {
             right.SwitchAndMove(this);
-        } else
-        {
+        } else {
             await Task.Delay(_delay);
 
             var handle = GetForegroundWindow();
@@ -194,13 +277,10 @@ partial class MainWindow
         }
     }
 
-    private async void Pin(object sender, RoutedEventArgs e)
-    {
-        if(this.ThisWindowMenu.IsChecked ?? true)
-        {
+    private async void Pin(object sender, RoutedEventArgs e) {
+        if(this.ThisWindowMenu.IsChecked ?? true) {
             this.TogglePin();
-        } else
-        {
+        } else {
             await Task.Delay(_delay);
 
             var handle = GetForegroundWindow();
@@ -208,63 +288,48 @@ partial class MainWindow
         }
     }
 
-    private async void PinApp(object sender, RoutedEventArgs e)
-    {
-        if(this.ThisWindowMenu.IsChecked ?? true)
-        {
+    private async void PinApp(object sender, RoutedEventArgs e) {
+        if(this.ThisWindowMenu.IsChecked ?? true) {
             Application.Current.TogglePin();
-        } else
-        {
+        } else {
             await Task.Delay(_delay);
 
-            if(VirtualDesktop.TryGetAppUserModelId(GetForegroundWindow(), out var appId))
-            {
+            if(VirtualDesktop.TryGetAppUserModelId(GetForegroundWindow(), out var appId)) {
                 (VirtualDesktop.IsPinnedApplication(appId) ? VirtualDesktop.UnpinApplication : (Func<string, bool>)VirtualDesktop.PinApplication)(appId);
             }
         }
     }
 
-    private void Remove(object sender, RoutedEventArgs e)
-    {
+    private void Remove(object sender, RoutedEventArgs e) {
         VirtualDesktop.Current.Remove();
     }
 
-    private void MoveLeft(object sender, RoutedEventArgs e)
-    {
-        foreach(var it in this.Desktops.Select((x, i) => new { Desktop = x, Index = i }))
-        {
-            if(it.Desktop.IsCurrent && it.Index > 0)
-            {
+    private void MoveLeft(object sender, RoutedEventArgs e) {
+        foreach(var it in this.Desktops.Select((x, i) => new { Desktop = x, Index = i })) {
+            if(it.Desktop.IsCurrent && it.Index > 0) {
                 Debug.WriteLine($"Moved: {it.Index} -> {it.Index - 1}");
                 VirtualDesktop.Move(VirtualDesktop.Current, it.Index - 1);
             }
         }
     }
 
-    private void MoveRight(object sender, RoutedEventArgs e)
-    {
-        foreach(var it in this.Desktops.Select((x, i) => new { Desktop = x, Index = i }))
-        {
-            if(it.Desktop.IsCurrent && (it.Index < (this.Desktops.Count - 1)))
-            {
+    private void MoveRight(object sender, RoutedEventArgs e) {
+        foreach(var it in this.Desktops.Select((x, i) => new { Desktop = x, Index = i })) {
+            if(it.Desktop.IsCurrent && (it.Index < (this.Desktops.Count - 1))) {
                 Debug.WriteLine($"Moved: {it.Index} -> {it.Index + 1}");
                 VirtualDesktop.Move(VirtualDesktop.Current, it.Index + 1);
             }
         }
     }
 
-    private void SwitchDesktop(object sender, RoutedEventArgs e)
-    {
-        if(sender is Button { DataContext: VirtualDesktopViewModel vm })
-        {
+    private void SwitchDesktop(object sender, RoutedEventArgs e) {
+        if(sender is Button { DataContext: VirtualDesktopViewModel vm }) {
             VirtualDesktop.FromId(vm.Id)?.SwitchAndMove(this);
         }
     }
 
-    private void ChangeWallpaper(object sender, RoutedEventArgs e)
-    {
-        if(sender is Button { DataContext: VirtualDesktopViewModel vm })
-        {
+    private void ChangeWallpaper(object sender, RoutedEventArgs e) {
+        if(sender is Button { DataContext: VirtualDesktopViewModel vm }) {
             var dialog = new OpenFileDialog()
             {
                 Title = "Select wallpaper",
@@ -272,8 +337,7 @@ partial class MainWindow
             };
 
             if((dialog.ShowDialog(this) ?? false)
-                && File.Exists(dialog.FileName))
-            {
+                && File.Exists(dialog.FileName)) {
                 var desktop = VirtualDesktop.FromId(vm.Id);
                 if(desktop != null)
                     desktop.WallpaperPath = dialog.FileName;
@@ -287,8 +351,7 @@ partial class MainWindow
     private static extern IntPtr GetForegroundWindow();
 }
 
-public class VirtualDesktopViewModel: INotifyPropertyChanged
-{
+public class VirtualDesktopViewModel: INotifyPropertyChanged {
     private string _name;
     private Uri? _wallpaperPath;
     private string _showcaseMessage;
@@ -298,68 +361,54 @@ public class VirtualDesktopViewModel: INotifyPropertyChanged
 
     public Guid Id { get; }
 
-    public string Name
-    {
+    public string Name {
         get => this._name;
-        set
-        {
-            if(this._name != value)
-            {
+        set {
+            if(this._name != value) {
                 this._name = value;
                 this.OnPropertyChanged();
             }
         }
     }
 
-    public Uri? WallpaperPath
-    {
+    public Uri? WallpaperPath {
         get => this._wallpaperPath;
-        set
-        {
-            if(this._wallpaperPath != value)
-            {
+        set {
+            if(this._wallpaperPath != value) {
                 this._wallpaperPath = value;
                 this.OnPropertyChanged();
             }
         }
     }
 
-    public string ShowcaseMessage
-    {
+    public string ShowcaseMessage {
         get => this._showcaseMessage;
-        set
-        {
-            if(this._showcaseMessage != value)
-            {
+        set {
+            if(this._showcaseMessage != value) {
                 this._showcaseMessage = value;
                 this.OnPropertyChanged();
             }
         }
     }
 
-    public bool IsCurrent
-    {
+    public bool IsCurrent {
         get => this._isCurrent;
-        set
-        {
-            if(this._isCurrent != value)
-            {
+        set {
+            if(this._isCurrent != value) {
                 this._isCurrent = value;
                 this.OnPropertyChanged();
             }
         }
     }
 
-    public VirtualDesktopViewModel(VirtualDesktop source)
-    {
+    public VirtualDesktopViewModel(VirtualDesktop source) {
         this._name = string.IsNullOrEmpty(source.Name) ? "(no name)" : source.Name;
         this._wallpaperPath = Uri.TryCreate(source.WallpaperPath, UriKind.Absolute, out var uri) ? uri : null;
         this._showcaseMessage = "";
         this.Id = source.Id;
     }
 
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) {
         this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
---
+++ samples/VirtualDesktop.Showcase/WindowTracker.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace VirtualDesktopShowcase {
+    internal struct WINDOWINFO {
+        public uint ownerpid;
+        public uint childpid;
+    }
+
+    public class WindowTrackerEvent: EventArgs {
+        public WindowTrackerEvent(IntPtr hwnd, string description = "") {
+            this.hwnd = hwnd;
+            this.description = description;
+        }
+
+        public IntPtr hwnd { get; }
+        public string description { get; }
+    }
+
+    internal class WindowTracker {
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType,
+            IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+        delegate bool EnumWindowsProc(IntPtr hwnd, int lParam);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr
+           hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess,
+           uint idThread, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+        [DllImport("user32.dll")]
+        static extern bool EnumWindows(EnumWindowsProc enumFunc, int lParam);
+
+        [DllImport("Oleacc.dll")]
+        static extern IntPtr GetProcessHandleFromHwnd(IntPtr hwnd);
+
+        [DllImport("kernel32.dll")]
+        static extern int GetProcessId(IntPtr handle);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetShellWindow();
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern bool IsWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        static extern bool IsWindowVisible(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        static extern bool IsZoomed(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        static extern bool IsIconic(IntPtr hwnd);
+
+        const uint OBJID_WINDOW = 0;
+        const uint EVENT_SYSTEM_FOREGROUND = 0x0003;
+        const uint EVENT_SYSTEM_MINIMIZESTART = 0x0016;
+        const uint EVENT_OBJECT_LOCATIONCHANGE = 0x800B;
+        const uint EVENT_OBJECT_DESTROY = 0x8001;
+        const uint WINEVENT_OUTOFCONTEXT = 0;
+
+        readonly HashSet<IntPtr> maxWindows = new();
+        readonly List<IntPtr> hhooks = new();
+
+        public delegate void EventHandler(object sender, WindowTrackerEvent e);
+        public event EventHandler<WindowTrackerEvent>? FloatWindowEvent;
+        public event EventHandler<WindowTrackerEvent>? MaximizeEvent;
+        public event EventHandler<WindowTrackerEvent>? UnmaximizeEvent;
+        public event EventHandler<WindowTrackerEvent>? MinimizeEvent;
+        public event EventHandler<WindowTrackerEvent>? CloseEvent;
+
+        public WindowTracker() {
+            hhooks.Add(SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero,
+                FloatWinEventHandler, 0, 0, WINEVENT_OUTOFCONTEXT));
+
+            hhooks.Add(SetWinEventHook(EVENT_OBJECT_LOCATIONCHANGE, EVENT_OBJECT_LOCATIONCHANGE, IntPtr.Zero,
+                MaxUnmaxWinEventHandler, 0, 0, WINEVENT_OUTOFCONTEXT));
+
+            hhooks.Add(SetWinEventHook(EVENT_SYSTEM_MINIMIZESTART, EVENT_SYSTEM_MINIMIZESTART, IntPtr.Zero,
+                MinWinEventHandler, 0, 0, WINEVENT_OUTOFCONTEXT));
+
+            hhooks.Add(SetWinEventHook(EVENT_OBJECT_DESTROY, EVENT_OBJECT_DESTROY, IntPtr.Zero,
+                CloseWinEventHandler, 0, 0, WINEVENT_OUTOFCONTEXT));
+        }
+
+        ~WindowTracker() {
+            foreach(var hhook in hhooks) {
+                UnhookWinEvent(hhook);
+            }
+        }
+
+        public void SortCurrentWindows() {
+            var shellWindow = GetShellWindow();
+            EnumWindows(delegate (IntPtr hwnd, int lParam) {
+                if(hwnd != shellWindow) {
+                    if(IsZoomed(hwnd)) {
+                        MaximizeEvent?.Invoke(this, new WindowTrackerEvent(hwnd, GetProcessDescriptionByHwnd(hwnd)));
+                    } else if(!IsIconic(hwnd)) {
+                        FloatWindowEvent?.Invoke(this, new WindowTrackerEvent(hwnd));
+                    }
+                }
+
+                return true;
+            }, 0);
+        }
+
+        void FloatWinEventHandler(IntPtr hWinEventHook, uint eventType,
+            IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime) {
+            if(idObject == OBJID_WINDOW &&
+               !IsZoomed(hwnd) &&
+               IsWindowVisible(hwnd) &&
+               GetWindowTextLength(hwnd) > 0) {
+                FloatWindowEvent?.Invoke(this, new WindowTrackerEvent(hwnd));
+            }
+        }
+
+        void MaxUnmaxWinEventHandler(IntPtr hWinEventHook, uint eventType,
+            IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime) {
+            if(idObject == OBJID_WINDOW && IsWindowVisible(hwnd)) {
+                if(!maxWindows.Contains(hwnd) &&
+                   IsZoomed(hwnd) &&
+                   GetWindowTextLength(hwnd) > 0) {
+                    maxWindows.Add(hwnd);
+                    MaximizeEvent?.Invoke(this, new WindowTrackerEvent(hwnd, GetProcessDescriptionByHwnd(hwnd)));
+                } else if(idObject == OBJID_WINDOW &&
+                          maxWindows.Contains(hwnd) &&
+                          !IsZoomed(hwnd) &&
+                          !IsIconic(hwnd)) {
+                    maxWindows.Remove(hwnd);
+                    UnmaximizeEvent?.Invoke(this, new WindowTrackerEvent(hwnd));
+                }
+            }
+        }
+
+        void MinWinEventHandler(IntPtr hWinEventHook, uint eventType,
+            IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime) {
+            if(maxWindows.Contains(hwnd)) {
+                maxWindows.Remove(hwnd);
+                MinimizeEvent?.Invoke(this, new WindowTrackerEvent(hwnd));
+            }
+        }
+
+        void CloseWinEventHandler(IntPtr hWinEventHook, uint eventType,
+            IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime) {
+            if(idObject == OBJID_WINDOW &&
+               maxWindows.Contains(hwnd) &&
+               IsZoomed(hwnd)) {
+                System.Threading.Thread.Sleep(100);
+                if(!IsWindow(hwnd)) {
+                    maxWindows.Remove(hwnd);
+                    CloseEvent?.Invoke(this, new WindowTrackerEvent(hwnd));
+                }
+            }
+        }
+
+        static string GetProcessDescriptionByHwnd(IntPtr hwnd) {
+            var procHandle = GetProcessHandleFromHwnd(hwnd);
+            if(procHandle == IntPtr.Zero) {
+                return hwnd.ToString();
+            }
+
+            var pid = GetProcessId(procHandle);
+            if(pid <= 0) {
+                return hwnd.ToString();
+            }
+
+            var process = Process.GetProcessById(pid);
+            var mainModule = process.MainModule;
+            if(mainModule == null) {
+                return hwnd.ToString();
+            }
+
+            string? fileDescription = mainModule.FileVersionInfo.FileDescription;
+            if("Application Frame Host".Equals(fileDescription)) {
+                return process.MainWindowTitle;
+            } else if(fileDescription == null) {
+                return hwnd.ToString();
+            }
+
+            return fileDescription;
+        }
+    }
+}
